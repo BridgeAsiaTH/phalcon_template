178441534df9bf52b247ca14eebb62b38b44ae93
kamol <kamol_c@bridgeasiagroup.com>
2018-08-31 22:15:27 +0700

    Use DIRECTORY_SEPARATOR instead

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@ use Phalcon\Di\FactoryDefault;
 
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
+
 
 try {
 
